cfc1f2ed46a6706ca23177c3d9522846e2714919
Corinne <hume.corinne@gmail.com>
2021-04-01 16:24:39 -0700

    Adding PyPoll_Challenge.py and .txt files

---
+++ Python_practice_2.py
@@ -0,0 +1,14 @@
+temperature = int(input("What is the temperature outside?"))
+
+if temperature > 80:
+    print("Turn on the AC.")
+else:
+    print("Open the windows.")
+#Import datetime class from datetime module
+import datetime as dt
+#Use now() attribute on dt class to get present time
+now=dt.datetime.now()
+#Print present time
+print("The time right now is", now)
+import random
+dir(random)
